97a1a369fe4642345671586e2e8e59c411da28d6
Apeksha Vinayak Bodade <apeksha@Apekshas-MacBook-Pro.local>
2024-07-16 18:23:33 -0400

    pass stage

--- app/server.go
+++ app/server.go
@@ -5,6 +5,9 @@ import (
 	// Uncomment this block to pass the first stage
 	"net"
 	"os"
+	"log"
+	// "bufio"
+	// "strings" 
 )
 
 func main() {
@@ -18,9 +21,31 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	defer conn.Close()
+	// for {
+		// reader := bufio.NewReader(conn)
+		// command, err := reader.ReadString('\n')
+		// command := make([]byte, 6)
+		// _, err := conn.Read(command)
+	buffer := make([]byte, 1024)
+	_, err = conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading from connection:", err.Error())
+		return
+	}
+	log.Printf("command: \n%s", string(buffer[8:12]))
+	// command = strings.TrimSpace(command)
+	// fmt.Println([]byte(buffer[:n]))
+	// fmt.Println(string(buffer))
+	if string(buffer[8:12]) == "PING" {
+		conn.Write([]byte("+PONG\r\n"))
+	} else {
+		conn.Write([]byte("-Err Unknown Command\r\n"))
+	}
+	// }
 }
---
+++ fixes.md
@@ -0,0 +1,7 @@
+Error:
+./spawn_redis_server.sh
+go: downloading go1.22 (darwin/arm64)
+go: download go1.22 for darwin/arm64: toolchain not available
+
+Resolution:
+change version to 1.22.0 from 1.22 in go.mod
