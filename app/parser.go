1008719aac9c9a922ee3c93150140824233c1e75
Apeksha Vinayak Bodade <apeksha@Apekshas-MacBook-Pro.local>
2024-07-18 17:43:54 -0400

    Added support for multiple commands

---
+++ app/parser.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"errors"
+	// "log"
+	"bytes"
+	// "bufio"
+	"strconv"
+	"fmt"
+)
+
+// Reference: https://redis-doc-test.readthedocs.io/en/latest/topics/protocol/#:~:text=RESP%20protocol%20description,-The%20RESP%20protocol&text=This%20is%20the%20protocol%20you,Integers%2C%20Bulk%20Strings%20and%20Arrays.
+
+const (
+	CLRF = "\r\n"
+)
+type Array []interface{}
+type BulkString struct {
+	Value  string
+	IsNull bool
+}
+type SimpleString string
+type Integer int
+
+func Parse(data []byte) (interface{}, []byte, error) {
+	dataType := data[0]
+	var (
+		parsed	interface{}
+		err		error
+	)
+	switch dataType {
+	case '*':
+		parsed, data, err = ParseArray(data)
+	case '$':
+		// BulkString
+		parsed, data, err = ParseString(data)
+	case ':':
+		parsed, data, err = ParseInteger(data)
+	default:
+		return nil, data, fmt.Errorf("unknown data type %b", dataType)
+	}
+
+	if err != nil {
+		return nil, data, fmt.Errorf("Error while parsing data", err)
+	}
+	return parsed, data, err
+}
+
+func ParseInteger(data []byte) (Integer, []byte, error) {
+	if len(data) < 4 {
+		return 0, data, errors.New("Integer needs at least 4 chracter")
+	}
+	data = data[1:]
+	hasSign := data[1] == '+' || data[1] == '-'
+	isNeg := data[1] == '-'
+	if hasSign {
+		data = data[1:]
+	}
+	integerData := bytes.SplitN(data, []byte(CLRF), 2)
+	if len(integerData) != 2 {
+		return 0, data, errors.New("integer needs end delimiter")
+	}
+	data = integerData[1]
+	integer, err := strconv.Atoi(string(integerData[0]))
+	if err != nil {
+		return 0, data, fmt.Errorf("value is not an integer: %s", integerData[0])
+	}
+	if isNeg {
+		integer = -integer
+	}
+	return Integer(integer), data, err
+}
+
+func ParseString(data []byte) (BulkString, []byte, error) {
+	bulkString := BulkString {
+		Value:  "",
+		IsNull: false,
+	}
+	if len(data) < 6 {
+		return bulkString, data, errors.New("String needs at least 6 chracters")
+	}
+	data = data[1:]
+	stringData := bytes.SplitN(data, []byte(CLRF), 2)
+	if len(stringData) != 2 {
+		return bulkString, data, errors.New("String needs a delimiter")
+	}
+	data = stringData[1]
+	length, err := strconv.Atoi(string(stringData[0]))
+	if err != nil {
+		return bulkString, data, fmt.Errorf("length is not an integer: %s", stringData[0])
+	}
+	if length == -1 {
+		bulkString.IsNull = true
+		return bulkString, data, nil
+	}
+	stringData = bytes.SplitN(data, []byte(CLRF), 2)
+	if len(stringData) != 2 {
+		return bulkString, data, errors.New("String needs a delimiter")
+	}
+	data = stringData[1]
+	bulkString.Value = string(stringData[0])
+	if err != nil {
+		return bulkString, data, fmt.Errorf("str is formed properly %s", stringData[0])
+	}
+	return bulkString, data, nil
+}
+
+func ParseArray(data []byte) (Array, []byte, error) {
+	if len(data) < 4 {
+		return nil, data, errors.New("array needs at least 4 chracter")
+	}
+	data = data[1:]
+	arraydata := bytes.SplitN(data, []byte(CLRF), 2)
+	if len(arraydata) != 2 {
+		return nil, data, errors.New("array needs start delimiter")
+	}
+	data = arraydata[1]
+	length, err := strconv.Atoi(string(arraydata[0]))
+	if err != nil {
+		return nil, data, fmt.Errorf("length is not an integer: %s", arraydata[0])
+	}
+	array := Array{}
+	for i := 0; i<length; i++ {
+		arrayItem, dataLeft, err := Parse(data)
+		if err != nil {
+			return nil, data, fmt.Errorf("failed to parse array item: %s, due to %w", data, err)
+		}
+		array = append(array, arrayItem)
+		data = dataLeft
+	}
+	return array, data, nil
+}
--- app/server.go
+++ app/server.go
@@ -8,7 +8,9 @@ import (
 	"io"
 	"errors"
 	// "log"
+	// "bytes"
 	// "bufio"
+	// "strconv"
 	// "strings" 
 )
 
@@ -35,6 +37,9 @@ func main() {
 	}
 	// return nil
 }
+func (d SimpleString) Encode() string {
+	return fmt.Sprintf("+%s\r\n", d)
+}
 func handleConn(conn net.Conn) {
 	defer conn.Close()
 	for {
@@ -48,16 +53,76 @@ func handleConn(conn net.Conn) {
 			// return err
 			os.Exit(1)
 		}
-		if string(buffer[8:12]) == "PING" {
+		// command := string(buffer)
+		// if string(buffer[0]) == "*" {
+		// 	array, _, err := ParseArray(buffer)
+		// 	if err != nil {
+		// 		os.Exit(1)
+		// 	}
+		// 	_, err = conn.Write([]byte("+" + array[1] + "\r\n"))
+		// 	// return array[1]
+		// }
+		parsedData, _, err := Parse(buffer)
+		if err != nil {
+			fmt.Println("Unable to parse the data")
+			os.Exit(1)
+		}
+		// if len(data) != 1 {
+		// 	fmt.Println("not all data are processed, data left: %b", data)
+		// 	os.Exit(1)
+		// }
+		arr, ok := parsedData.(Array)
+		if !ok {
+			fmt.Println("data should be an array")
+			os.Exit(1)
+		}
+		command, ok := arr[0].(BulkString)
+		if !ok {
+			fmt.Println("Command should be a string")
+			os.Exit(1)
+		}
+		args := Array{}
+		if len(arr) > 1 {
+			args = arr[1:]
+		}
+		if command.Value == "PING" {
+			fmt.Println("came here.........")
+			// fmt.Println("data: ", string(data))
 			_, err = conn.Write([]byte("+PONG\r\n"))
 			if err != nil {
 				fmt.Println(err, "Write response")
 				// return err
 				os.Exit(1)
 			}
+		} else if command.Value == "ECHO" {
+			// if len(command) < 2 {
+			// 	os.Exit(1)
+			// }
+			// _, err = conn.Write([]byte("+PONG\r\n"))
+			// fmt.Println("data: ", string(data))
+			arr, ok := args[0].(BulkString)
+			if !ok {
+				fmt.Println("args[0] should be a BulkString")
+				os.Exit(1)
+			}
+			_, err = conn.Write([]byte(SimpleString(arr.Value).Encode()))
+			if err != nil {
+				fmt.Println(err, "Write Response")
+				os.Exit(1)
+			}
 		} else {
 			conn.Write([]byte("-Err Unknown Command\r\n"))
 		}
+		// if string(buffer[8:12]) == "PING" {
+		// 	_, err = conn.Write([]byte("+PONG\r\n"))
+		// 	if err != nil {
+		// 		fmt.Println(err, "Write response")
+		// 		// return err
+		// 		os.Exit(1)
+		// 	}
+		// } else {
+		// 	conn.Write([]byte("-Err Unknown Command\r\n"))
+		// }
 	}
 	// return err
 }
