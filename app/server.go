7b7fce44faf7292f5dc3e16af5086b8033aafa3c
Apeksha Vinayak Bodade <apeksha@Apekshas-MacBook-Pro.local>
2024-07-22 15:19:10 -0400

    .

--- app/server.go
+++ app/server.go
@@ -181,7 +181,7 @@ func handleConn(store *Storage, conn net.Conn, info map[string]string) {
 		case "PSYNC":
 			// fmt.Println(SimpleString("FULLRESYNC" + info["master_replid"] + info["master_repl_offset"]).Encode())
 			conn.Write([]byte(SimpleString("FULLRESYNC " + info["master_replid"] + " " + info["master_repl_offset"]).Encode()))
-			emptyrdb := "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2"
+			emptyrdb := hex.DecodeString("524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2")
 			time.Sleep(1 * time.Second)
 			conn.Write([]byte("$" + strconv.Itoa(len(emptyrdb)) + "\r\n" + string(emptyrdb)))
 			break
