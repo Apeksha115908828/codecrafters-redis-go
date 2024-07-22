59f1efae28db4600be09633bbc4e95a3a9293e38
Apeksha Vinayak Bodade <apeksha@Apekshas-MacBook-Pro.local>
2024-07-22 14:18:59 -0400

    .

--- app/server.go
+++ app/server.go
@@ -61,7 +61,7 @@ func sendHandshake(info map[string]string) (){
 		os.Exit(1)
 	}
 	replica.Write([]byte("*1\r\n$4\r\nPING\r\n"))
-	replica.Write([]byte("*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n"+ info["port"] +"\r\n"))
+	replica.Write([]byte("*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n6380\r\n"))
 	replica.Write([]byte("*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n"))
 }
 
