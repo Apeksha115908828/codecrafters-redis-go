67c27466f0953b69a7fcb126b49b35051fd57dac
Apeksha Vinayak Bodade <apeksha@Apekshas-MacBook-Pro.local>
2024-07-22 15:12:45 -0400

    .

--- app/server.go
+++ app/server.go
@@ -13,6 +13,7 @@ import (
 	"strconv"
 	"strings" 
 	"time"
+	"encoding/hex"
 )
 
 func main() {
@@ -180,6 +181,8 @@ func handleConn(store *Storage, conn net.Conn, info map[string]string) {
 		case "PSYNC":
 			// fmt.Println(SimpleString("FULLRESYNC" + info["master_replid"] + info["master_repl_offset"]).Encode())
 			conn.Write([]byte(SimpleString("FULLRESYNC " + info["master_replid"] + " " + info["master_repl_offset"]).Encode()))
+			emptyrdb := "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2"
+			conn.Write([]byte("$" + strconv.Itoa(len(emptyrdb)) + "\r\n" + string(emptyrdb)))
 			break
 		case "ECHO":
 			handleEcho(conn, args)
