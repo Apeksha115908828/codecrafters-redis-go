f65313a23a8c477fc8d520378c298eff4e238495
Apeksha Vinayak Bodade <apeksha@Apekshas-MacBook-Pro.local>
2024-07-22 12:49:02 -0400

    .

--- app/server.go
+++ app/server.go
@@ -20,7 +20,11 @@ func main() {
 
 	// Uncomment this block to pass the first stage
 	//
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	port := "6379"
+	if len(os.Args) > 1 {
+		port = os.Args[1]
+	}
+	l, err := net.Listen("tcp", "0.0.0.0:" + port)
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
