36417b54932643714e69ba8233580c8bfe5fa512
Apeksha Vinayak Bodade <apeksha@Apekshas-MacBook-Pro.local>
2024-09-22 23:43:28 -0400

    .

--- app/server.go
+++ app/server.go
@@ -132,7 +132,7 @@ func handleReplica(store *Storage, info map[string]string) {
 				// 		conn.Write([]byte(SimpleString("OK").Encode()))
 				// 	}
 				// 	break
-
+				ackChannel <- true
 				offset := strconv.Itoa(replicaOffset)
 				lengthoffset := len(offset)
 				response := "*3\r\n$8\r\nREPLCONF\r\n$3\r\nACK\r\n$" + strconv.Itoa(lengthoffset) + "\r\n" + offset + "\r\n"
@@ -354,7 +354,7 @@ func handleConn(store *Storage, conn net.Conn, info map[string]string, replicas 
 				lengthoffset := len(offset)
 				response := "*3\r\n$8\r\nREPLCONF\r\n$3\r\nACK\r\n$" + strconv.Itoa(lengthoffset) + "\r\n" + offset + "\r\n"
 				conn.Write([]byte(response))
-				ackChannel <- true
+				// ackChannel <- true
 			} else {
 				conn.Write([]byte(SimpleString("OK").Encode()))
 			}
